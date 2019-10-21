b14c82151187c0160e4e237d438a5c1306aaf423
Henry Onyango <henry.onyango@outlook.com>
2019-10-21 16:02:38 +0300

    Create main view model class for blog repository

--- app/src/main/java/com/example/livedataexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/livedataexample/viewmodel/MainViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.livedataexample.viewmodel
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.ViewModel
+import com.example.livedataexample.model.Blog
+
+class MainViewModel(): ViewModel() {
+    val blogRepository = BlogRepository()
+    val allBlogs : LiveData<List<Blog>>
+        get() = blogRepository.getMutableLiveData()
+
+    override fun onCleared() {
+        super.onCleared()
+        blogRepository.completableJob.cancel()
+    }
+}
